d149cbc561a9b961d50f5b4a49897701dd88776e
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-02-13 12:14:06 +0100

    Remove deprecated DTO's

--- src/main/java/org/dcistudent/geolocationstatisticsapi/dto/AsDto.java
+++
@@ -1,3 +0,0 @@
-package org.dcistudent.geolocationstatisticsapi.dto;
-
-public record AsDto(Long id, Long asNumber, String asName, String ipStart, String ipEnd) {}
--- src/main/java/org/dcistudent/geolocationstatisticsapi/dto/IpBlocksPerCountryDto.java
+++
@@ -1,3 +0,0 @@
-package org.dcistudent.geolocationstatisticsapi.dto;
-
-public record IpBlocksPerCountryDto(String country, Long count) {}
