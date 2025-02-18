474e68c1d45c0723a6e42dc075dfa2b9b7299eeb
DCI - Marc <marcel.saegebarth@dci-student.org>
2025-02-18 11:02:25 +0100

    Rename data model namespace

--- src/main/java/org/dcistudent/geolocationstatisticsapi/managers/AsnManager.java
+++ src/main/java/org/dcistudent/geolocationstatisticsapi/managers/AsnManager.java
@@ -2,7 +2,7 @@ package org.dcistudent.geolocationstatisticsapi.managers;
 
 import org.dcistudent.geolocationstatisticsapi.entities.Asn;
 import org.dcistudent.geolocationstatisticsapi.mappers.manager.AsnDataModelMapper;
-import org.dcistudent.geolocationstatisticsapi.models.data.asn.DataModel;
+import org.dcistudent.geolocationstatisticsapi.models.data.AsnDataModel;
 import org.dcistudent.geolocationstatisticsapi.repositories.AsnRepository;
 import org.springframework.stereotype.Service;
 
@@ -21,22 +21,22 @@ public class AsnManager {
     this.mapper = mapper;
   }
 
-  public Map<Long, DataModel> findAsByNumber(Long asNumber) {
+  public Map<Long, AsnDataModel> findAsByNumber(Long asNumber) {
     List<Asn> list = asnRepository.findAsByNumber(asNumber);
     return this.convertToDataModel(list);
   }
 
-  public Map<Long, DataModel> findAsById(Long id) {
+  public Map<Long, AsnDataModel> findAsById(Long id) {
     List<Asn> list = asnRepository.findAsById(id);
     return this.convertToDataModel(list);
   }
 
-  public Map<Long, DataModel> findAsByName(String name) {
+  public Map<Long, AsnDataModel> findAsByName(String name) {
     List<Asn> list = asnRepository.findAsByName(name);
     return this.convertToDataModel(list);
   }
 
-  private Map<Long, DataModel> convertToDataModel(List<Asn> list) {
+  private Map<Long, AsnDataModel> convertToDataModel(List<Asn> list) {
     return list.stream().collect(
         Collectors.toMap(
             Asn::getAsNumber,
--- src/main/java/org/dcistudent/geolocationstatisticsapi/mappers/manager/AsnDataModelMapper.java
+++ src/main/java/org/dcistudent/geolocationstatisticsapi/mappers/manager/AsnDataModelMapper.java
@@ -1,22 +1,22 @@
 package org.dcistudent.geolocationstatisticsapi.mappers.manager;
 
 import org.dcistudent.geolocationstatisticsapi.entities.Asn;
-import org.dcistudent.geolocationstatisticsapi.models.data.asn.DataModel;
-import org.dcistudent.geolocationstatisticsapi.models.data.asn.IpBlockDataModel;
+import org.dcistudent.geolocationstatisticsapi.models.data.AsnDataModel;
+import org.dcistudent.geolocationstatisticsapi.models.data.IpBlockDataModel;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AsnDataModelMapper {
 
-  DataModel mapToDataModel(Asn asn);
+  AsnDataModel mapToDataModel(Asn asn);
 
   @Mapping(source = "ipStart", target = "ipStart")
   @Mapping(source = "ipEnd", target = "ipEnd")
   IpBlockDataModel mapToAsnIpBlocksDataModel(Asn asn);
 
-  default DataModel mapToDataModelWithIpBlocks(Asn asn) {
-    DataModel model = this.mapToDataModel(asn);
+  default AsnDataModel mapToDataModelWithIpBlocks(Asn asn) {
+    AsnDataModel model = this.mapToDataModel(asn);
     model.getIpBlocks().add(this.mapToAsnIpBlocksDataModel(asn));
     return model;
   }
